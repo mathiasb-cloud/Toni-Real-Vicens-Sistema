d3bdf19224bc8bb9e89e094cf152114a14083323
Mathias Benites <benitesmathiaa@gmail.com>
2026-07-01 22:30:17 -0500

    Adjust CitaService declaration in HomeController

--- Toni-Real-Vicens-Sistema/Controllers/HomeController.cs
+++ Toni-Real-Vicens-Sistema/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace Toni_Real_Vicens_Sistema.Controllers
     {
         private readonly ILogger<HomeController> _logger;
         private readonly AlumnoService _alumnoService; 
-        private readonly CitaService _citaService;     // Ajusta el nombre según tu servicio
+        private readonly CitaService _citaService;     
         private readonly FichaService _fichaService;   // Ajusta el nombre según tu servicio
 
         public HomeController(ILogger<HomeController> logger, AlumnoService alumnoService, CitaService citaService, FichaService fichaService)
